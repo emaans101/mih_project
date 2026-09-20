70ad86264fbf3b3489f1bf60450908ae64f3b2d8
Emaan Fatima <emaanfattima933@gmail.com>
2026-09-20 20:00:59 +0400

    Modify Vite config to optimize dependencies
    Updated Vite configuration to include optimizeDeps.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mih_project/',
   plugins: [react()],
+  base: '/mih_project/',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
